af5a58b171c29d9f599385d07b658ea99f55abeb
Paulo Ferreira <pauloaugusto@id.uff.br>
2023-06-17 12:09:18 -0300

    FIX - running rubocop and correcting offenses

--- spec/request/sessions_spec.rb
+++ spec/request/sessions_spec.rb
@@ -6,27 +6,27 @@ RSpec.describe '/sessions', type: :request do
   describe 'POST #create' do
     context 'with valid credentials' do
       it 'returns a JWT token' do
-        post '/api/v1/login', params: 
-        { 
-          email: user.email, 
-          password: 'password' 
+        post '/api/v1/login', params:
+        {
+          email: user.email,
+          password: 'password'
         }, headers: headers_json, as: :json
 
         expect(response).to have_http_status(:success)
 
         body = JSON.parse(response.body)
         expect(body['token']).not_to be_nil
-     end
+      end
     end
 
     context 'with invalid credentials' do
       it 'returns an error message' do
-        post '/api/v1/login', params: 
-        { 
-          email: user.email, 
-          password: 'wrong_password' 
+        post '/api/v1/login', params:
+        {
+          email: user.email,
+          password: 'wrong_password'
         }, headers: headers_json, as: :json
-        
+
         expect(response).to have_http_status(:unauthorized)
 
         body = JSON.parse(response.body)
